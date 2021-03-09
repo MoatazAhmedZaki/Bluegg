4c4c91b0f92eec1962196789b0d2039ad9e529d7
MoatazAhmedZaki <moatazcs@gmailcom>
2021-03-09 15:29:38 +0200

    Updates

---
+++ precache-manifest.93c24675f2db463001dae1b2ed4bf63a.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0faf5afe956ed127fcd25c60742dc05a",
+    "url": "/Bluegg/index.html"
+  },
+  {
+    "revision": "89634d211fca09b318df",
+    "url": "/Bluegg/static/css/main.ac3ba8b6.chunk.css"
+  },
+  {
+    "revision": "96eea14f415d2b8cc3ea",
+    "url": "/Bluegg/static/js/2.99289c99.chunk.js"
+  },
+  {
+    "revision": "89634d211fca09b318df",
+    "url": "/Bluegg/static/js/main.c2660eb3.chunk.js"
+  },
+  {
+    "revision": "ba007d1b81575b01dc24",
+    "url": "/Bluegg/static/js/runtime-main.b75c8feb.js"
+  },
+  {
+    "revision": "571b855fad4ef75d55fa059bc8e92687",
+    "url": "/Bluegg/static/media/3chillies-thumbnail.571b855f.jpg"
+  },
+  {
+    "revision": "edb33a68bbb796b02933352a2b5f8d05",
+    "url": "/Bluegg/static/media/Llanech.edb33a68.jpg"
+  },
+  {
+    "revision": "62aaba7668fda55f75837280cf992fb9",
+    "url": "/Bluegg/static/media/TSC-hero.62aaba76.jpg"
+  },
+  {
+    "revision": "5a57fc6cb03f53a5146a3e12cb7702c4",
+    "url": "/Bluegg/static/media/afr.5a57fc6c.png"
+  },
+  {
+    "revision": "82202e76dd8a32e0e079815d80042ffe",
+    "url": "/Bluegg/static/media/ap-project-cover.82202e76.jpg"
+  },
+  {
+    "revision": "c44fe604953c31264785350e7b8f09f5",
+    "url": "/Bluegg/static/media/bluegg.c44fe604.jpg"
+  },
+  {
+    "revision": "a25f0a5e94d4d1ee8790b9a8a3d07a28",
+    "url": "/Bluegg/static/media/branding.a25f0a5e.gif"
+  },
+  {
+    "revision": "c58a8f62e6cb0ae1d4759e50072da314",
+    "url": "/Bluegg/static/media/candleston (1).c58a8f62.jpg"
+  },
+  {
+    "revision": "0c3eb86484adce3ea3915ffe0b28341f",
+    "url": "/Bluegg/static/media/candleston (2).0c3eb864.jpg"
+  },
+  {
+    "revision": "26c090b2046453db58490a4890a134a0",
+    "url": "/Bluegg/static/media/candleston (3).26c090b2.jpg"
+  },
+  {
+    "revision": "7d437091d2a31204a7afc5620d4c666a",
+    "url": "/Bluegg/static/media/carercake.7d437091.jpg"
+  },
+  {
+    "revision": "48c1d193dc6d4f55319024856feddeef",
+    "url": "/Bluegg/static/media/cover-2.48c1d193.jpg"
+  },
+  {
+    "revision": "472be70ee709c294836fcc13ab90b65c",
+    "url": "/Bluegg/static/media/craft-commerce.472be70e.svg"
+  },
+  {
+    "revision": "5a6f12190a86dea5b1c8085662b273c0",
+    "url": "/Bluegg/static/media/craft.5a6f1219.svg"
+  },
+  {
+    "revision": "550ab80a795a062a64296ca232f8dbdf",
+    "url": "/Bluegg/static/media/digital.550ab80a.svg"
+  },
+  {
+    "revision": "dfe509158c79a12973adc1ec93b97b0d",
+    "url": "/Bluegg/static/media/digital.dfe50915.png"
+  },
+  {
+    "revision": "853e8f4d1e8c2164ae95f0f398d7c1d3",
+    "url": "/Bluegg/static/media/dog1.853e8f4d.jpg"
+  },
+  {
+    "revision": "251358ee2f2bb082daaf25f59ca5dc18",
+    "url": "/Bluegg/static/media/dog2.251358ee.jpg"
+  },
+  {
+    "revision": "c0c3cd243c259c61e129f7e4cd4ac3d6",
+    "url": "/Bluegg/static/media/fly.c0c3cd24.png"
+  },
+  {
+    "revision": "4f79d9eb9fb661d726595af850665712",
+    "url": "/Bluegg/static/media/logo.4f79d9eb.svg"
+  },
+  {
+    "revision": "18cf5f0b1c4c7f0237082c091a383b32",
+    "url": "/Bluegg/static/media/mazuma-hero.18cf5f0b.jpg"
+  },
+  {
+    "revision": "dba2eca1e9da6b205cdad46bd654a9e7",
+    "url": "/Bluegg/static/media/pic2.dba2eca1.jpg"
+  },
+  {
+    "revision": "802906ac52b9cacde938fc5fbced2008",
+    "url": "/Bluegg/static/media/pic3.802906ac.jpg"
+  },
+  {
+    "revision": "777302bb64722bf58669048dca70ea7c",
+    "url": "/Bluegg/static/media/shopify.777302bb.svg"
+  },
+  {
+    "revision": "22a233277cf6d3df06f6d8bcb8f9d8db",
+    "url": "/Bluegg/static/media/shout_5.22a23327.gif"
+  },
+  {
+    "revision": "8fbe3bbdef772784c5561102bc996bef",
+    "url": "/Bluegg/static/media/sisonai.8fbe3bbd.jpg"
+  },
+  {
+    "revision": "82bcfb6c5868e36caa59950d081244aa",
+    "url": "/Bluegg/static/media/sudeley-castle-cover.82bcfb6c.jpg"
+  },
+  {
+    "revision": "1f79d31331634254acb7ea6c0eecbf12",
+    "url": "/Bluegg/static/media/team.1f79d313.jpg"
+  },
+  {
+    "revision": "84307eaaaaae66b15fb78bb3714cbc01",
+    "url": "/Bluegg/static/media/team2.84307eaa.jpg"
+  },
+  {
+    "revision": "452fbda5e491b27c570cad60a061db9d",
+    "url": "/Bluegg/static/media/team3.452fbda5.jpg"
+  },
+  {
+    "revision": "61a2d693d485b4759331f7fcb2088a5a",
+    "url": "/Bluegg/static/media/team4.61a2d693.jpg"
+  },
+  {
+    "revision": "34b496b74aa4d399703ca40ce05fc3d2",
+    "url": "/Bluegg/static/media/tom-mike.34b496b7.jpg"
+  },
+  {
+    "revision": "14955fb07d46e7704cd39ab603afb074",
+    "url": "/Bluegg/static/media/uncle.14955fb0.jpg"
+  },
+  {
+    "revision": "6f8c8f1122b02d124d631ff6a055902f",
+    "url": "/Bluegg/static/media/vision.6f8c8f11.png"
+  },
+  {
+    "revision": "1909d1e080ffc7c448bb877a3e04c186",
+    "url": "/Bluegg/static/media/web-design.1909d1e0.gif"
+  },
+  {
+    "revision": "df5898a0449b10d34496f43299a50250",
+    "url": "/Bluegg/static/media/whitgift-care-thumb.df5898a0.jpg"
+  }
+]);
